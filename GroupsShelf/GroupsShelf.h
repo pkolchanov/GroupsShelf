72e9c5dc893aff5814bfce8eff7359a6d2cacb42
Pavel Kolchanov <hi@pkolchanov.ru>
2025-02-17 13:26:54 +0500

    Initial Commit

---
+++ GroupsShelf/GroupsShelf.h
@@ -0,0 +1,15 @@
+//
+//  GroupsShelf.h
+//  GroupsShelf
+//
+//  Created by pkolchanov on 17.02.2025.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <GlyphsCore/GlyphsPluginProtocol.h>
+
+@interface GroupsShelf : NSObject <GlyphsPlugin>
+
+@end
---
+++ GroupsShelf/GroupsShelf.m
@@ -0,0 +1,30 @@
+//
+//  GroupsShelf.m
+//  GroupsShelf
+//
+//  Created by pkolchanov on 17.02.2025.
+//
+//
+
+#import "GroupsShelf.h"
+
+@implementation GroupsShelf
+
+- (instancetype) init {
+	self = [super init];
+	if (self) {
+		// do stuff
+	}
+	return self;
+}
+
+- (NSUInteger)interfaceVersion {
+	// Distinguishes the API verison the plugin was built for. Return 1.
+	return 1;
+}
+
+- (void)loadPlugin {
+	// Set up stuff
+}
+
+@end
